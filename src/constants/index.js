5f1992893abaf4ad771898a8f6f6735578274039
Shefa <133137439+Shefaoudeen@users.noreply.github.com>
2023-09-16 16:06:27 +0530

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -334,7 +334,6 @@ const literary = [
       "INDIVIDUAL EVENT",
       "Participants will step into the shoes of an author or director in this engaging written event.",
       "They'll be presented with a captivating story and tasked with crafting their own climactic endings.",
-      "Whether they choose to evoke tears or send shivers down spines, the stage is theirs to command.",
       "Registration fee is Rs. 50.",
       "Coordinators: Hrithin -8156881685, Nivedha P - 8438737345",
     ],
